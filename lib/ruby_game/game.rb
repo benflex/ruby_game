103b1e132c640a6d56860cc5fe764a709cb39029
benflex <breverend@leadformance.com>
2013-12-10 17:07:47 +0100

    Using blocs and yield

--- lib/ruby_game/game.rb
+++ lib/ruby_game/game.rb
@@ -2,15 +2,11 @@ require 'gosu'
 
 module RubyGame
   class Game < Gosu::Window
-    def initialize(ruby, player)
+    def initialize
       super(640, 480, false)
       self.caption = "Ruby Game"
       @background_image = Gosu::Image.new(self, File.join(IMAGES_PATH, 'background.png'), true)
       @font = Gosu::Font.new(self, Gosu::default_font_name, 60)
-      @ruby = ruby
-      @ruby.init_image(self)
-      @player = player
-      @player.init_image(self)
     end
     
     def update
@@ -26,14 +22,16 @@ module RubyGame
     
     def draw
       @background_image.draw(0, 0, 0)
-      @font.draw("j00 Roxor!!", 200, 240, 2, 1.0, 1.0, 0xffffff00) if self.won?
+      @font.draw("j00 Roxor!!", 180, 240, 2, 1.0, 1.0, 0xffffff00) if self.won?
       @ruby.draw
       @player.draw
     end
     
     def start!
+      yield(self)
       @state = :run
       self.show
+      
     end
     
     def run?
@@ -47,5 +45,16 @@ module RubyGame
     def won?
       @state == :won
     end
+    
+    def player (player)
+      @player = player
+      @player.init_image(self)
+    end
+    
+    def ruby (ruby)
+      @ruby = ruby
+      @ruby.init_image(self)
+    end
+    
   end
 end
